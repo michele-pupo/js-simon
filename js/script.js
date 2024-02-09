4da0f05ded3083a379e0035d41c019189945a6a7
michele-pupo <mikypupo@hotmail.it>
2024-02-09 14:28:24 +0100

    create constants with local time and chosen date

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@
 // - Quanti millisecondi mi separano da domani alle 9:30?
 // - Esiste un oggetto JS in grado di gestire le date?
 // - Esistono dei metodi per trasformare una data in millisecondi?
+
+// creiamo una variabile che ci dia l'ora esatta in questo momento tramite una stringa
+const now = new Date();
+console.log(now);
+
+// creiamo una variabile con l'ora ed il giorno scelto da noi
+const chosenDate = new Date('2024-02-12T09:30:00');
+console.log(chosenDate);
+  
+    
